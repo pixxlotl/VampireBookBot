ce01b29dd9650c4fb90fab7d5e9074313a10b208
pixxlotl <pixxlotl@gmail.com>
2025-11-20 18:54:09 -0800

    Create MessageListener.java

---
+++ MessageListener.java
@@ -0,0 +1,13 @@
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+ 
+public class MessageListener extends ListenerAdapter {
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) return;
+        String message = event.getMessage().getContentRaw();
+        if (message.equals("!hello")) {
+            event.getChannel().sendMessage("Hello!").queue();
+        }
+    }
+}
