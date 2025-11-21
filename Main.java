b20fbf18436228b5ab704d7019f684fbaf5d0d77
pixxlotl <pixxlotl@gmail.com>
2025-11-20 18:53:47 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+ 
+import javax.security.auth.login.LoginException;
+ 
+public class Main {
+    public static void main(String[] args) throws LoginException {
+        String token = "YOUR_DISCORD_BOT_TOKEN";
+        JDA jda = JDABuilder.createDefault(token)
+               .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+               .disableCache(CacheFlag.VOICE_STATE)
+               .setActivity(Activity.playing("Java Bot"))
+               .build();
+    }
+}
